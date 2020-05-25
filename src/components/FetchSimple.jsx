fd2c843d52982095b53732e43e6d3511e096a825
David Andersson <lejonmanen@gmail.com>
2020-05-25 13:45:11 +0200

    Simple fetch fixed, using Chuck Norris API

--- src/components/FetchSimple.jsx
+++ src/components/FetchSimple.jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
 const FetchSimple = () => {
-    const url = 'https://forverkliga.se/JavaScript/api/simple.php';
+    const url = 'https://api.chucknorris.io/jokes/random';
     const [data, setData] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
-            console.log('About to send request: ', url);
+            // console.log('About to send request: ', url);
             const response = await fetch(url);
             const json = await response.json();
-            console.log('Fetched data:', json);
+            // console.log('Fetched data:', json);
+            setData({ image: json.icon_url, text: json.value });
         }
         fetchData();
     }, [])
     return (
-        <div> {data ? 'DATA' : 'no data'} </div>
+        <div> {data
+            ? <> <img src={data.image} alt="Chuck face" /> {data.text} </>
+            : 'no data'} </div>
     );
 }
 
